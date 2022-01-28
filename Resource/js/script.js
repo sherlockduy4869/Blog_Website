e164a6278ce3bd727cbd2adeb8fee6dbb15aeb9b
Duy-Kun <sherlockduy2001@gmail.com>
2022-01-28 07:59:28 +0700

    Code sticky navigation and scroll effect

---
+++ Resource/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(
+    function(){
+
+        //sticky nav
+        $('.about-section').waypoint(
+            function(direction){
+                if(direction== "down"){
+                    $('nav').addClass('sticky');
+                }
+                else{
+                    $('nav').removeClass('sticky');
+                }
+            },{
+                offset: '600px'
+            }
+        )
+
+        //scroll
+        $('a').click(function(event){
+            $('html, body').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 600);
+            event.preventDefault();
+        });
+    }
+)
+    
