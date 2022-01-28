a575388af874093f43b498c580d16b5e6f350cd8
Duy-Kun <sherlockduy2001@gmail.com>
2022-01-28 11:32:27 +0700

    Code mobile navigation

--- Resource/js/script.js
+++ Resource/js/script.js
@@ -16,12 +16,29 @@ $(document).ready(
         )
 
         //scroll
-        $('a').click(function(event){
+        $('a').click(
+            function(event){
             $('html, body').animate({
                 scrollTop: $( $.attr(this, 'href') ).offset().top
             }, 600);
             event.preventDefault();
         });
+
+        //mobile navigation
+        $('.mobile-nav-icon').click(
+            function(){
+                $('.main-nav').slideToggle(200);
+
+                if($('.mobile-nav-icon').hasClass('fa-bars')){
+                    $('.mobile-nav-icon').addClass('fa-times');
+                    $('.mobile-nav-icon').removeClass('fa-bars');
+                }
+                else{
+                    $('.mobile-nav-icon').addClass('fa-bars');
+                    $('.mobile-nav-icon').removeClass('fa-times');
+                }
+            }
+        )
     }
 )
     
